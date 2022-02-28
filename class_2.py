7f3764820c8a8348aa7ff8893d619a4480c351fa
DeKamatsu <denis_matveev@tut.by>
2022-02-28 20:48:00 +0300

    2-nd commit

---
+++ class_2.py
@@ -0,0 +1,20 @@
+# 2-nd lesson. Variables. I/O
+
+print (round(1.1245, 4))
+name = 'fdgh\n'
+print(name * 5)
+print (name[-2])
+print (name[2:])
+print (name[:2])
+
+print(len(name))
+
+#print(format("String is %name"))
+#print(f"String is {name}")
+
+if not(1 != 1):
+    print(1)
+else:
+    print(2)
+
+d = 'sdfhedhj'.count('e')
